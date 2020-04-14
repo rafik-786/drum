1a1912ef4ecd3d25158abd109a98d454008e10ac
rafik-786 <61990438+rafik-786@users.noreply.github.com>
2020-04-14 19:48:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,62 @@
 var noDrum = document.querySelectorAll(".drum").length;
 for (var i = 0; i < noDrum; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 }
 
 function handleClick() {
-    var clicked = this.innerHTML;
-    soundBox(clicked);
+  var clicked = this.innerHTML;
+  soundBox(clicked);
+  animate(clicked);
 }
 
 function soundBox(keyitem) {
-    var audio = new Audio();
-    switch (keyitem) {
-        case "w":
-            audio = new Audio("sounds/crash.mp3");
-            audio.play();
-            break;
-        case "a":
-            audio = new Audio("sounds/kick-bass.mp3");
-            audio.play();
-            break;
-        case "s":
-            audio = new Audio("sounds/snare.mp3");
-            audio.play();
-            break;
+  var audio;
+  switch (keyitem) {
+    case "w":
+      audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "a":
+      audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "s":
+      audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
 
-        case "d":
-            audio = new Audio("sounds/tom-1.mp3");
-            audio.play();
-            break;
-        case "j":
-            audio = new Audio("sounds/tom-2.mp3");
-            audio.play();
-            break;
-        case "k":
-            audio = new Audio("sounds/tom-3.mp3");
-            audio.play();
-            break;
-        case "l":
-            audio = new Audio("sounds/tom-4.mp3");
-            audio.play();
-            break;
+    case "d":
+      audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "j":
+      audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "k":
+      audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "l":
+      audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
 
-        default:
-            console.log(clicked);
-            break;
-    }
+    default:
+      console.log(keyitem);
+      break;
+  }
 }
 document.addEventListener("keypress", keyboard);
 
 function keyboard() {
-    soundBox(event.key);
+  soundBox(event.key);
+  animate(event.key);
+}
+function animate(currentkey) {
+  var active = document.querySelector("." + currentkey);
+  active.classList.add("pressed");
+  setTimeout(function () {
+    active.classList.remove("pressed");
+  }, 100);
 }
